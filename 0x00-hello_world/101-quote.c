fafbed3bda344608cad66a1d34bffdf46d91d35a
yemanekb <yemanekb@gmail.com>
2022-10-13 12:50:44 -0700

    Task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,22 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- * Return:1
-*/
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
 {
 
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+
+			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 
 	return (1);
 

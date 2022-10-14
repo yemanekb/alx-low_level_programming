62e528d0fc64cd5f94ef827e60932508d312ce1f
yemanekb <yemanekb@gmail.com>
2022-10-14 15:48:59 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+int main(void)
+
+{
+
+		int n = 48;
+
+
+
+			while (n < 58)
+
+					{
+
+								putchar(n);
+
+										n++;
+
+											}
+
+				putchar ('\n');
+
+
+
+					return (0);
+
+}

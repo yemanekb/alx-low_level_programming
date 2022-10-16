9187aa24aac49601f0c63d3d624b365eb6f3edd7
yemanekb <113693054+yemanekb@users.noreply.github.com>
2022-10-16 17:56:38 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,69 +1,44 @@
 #include <stdio.h>
-
-
-
+#include <unistd.h>
 /**
- *
- *  * main - main - two pairs of numbers that do not repeat
- *
- *   *
- *
- *    * Return (0)
- *
- *     */
-
-
-
+ * main - Entry point
+ * Description: prints all possible combination of 2-digit numbers
+ * Return: Always 0 (success)
+ */
 int main(void)
-
 {
+	int c, i, k, j;
 
-		int i, j;
+	for (c = 48; c <= 57; c++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+				if (((k + j) > (c + i) &&  k >= c) || c < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(' ');
+					putchar(k);
+					putchar(j);
 
-
-
-			for (j = 0; j <= 99; j++)
-
+					if (c + i + k + j == 227 && c == 57)
 					{
-
-								for (i = j; i <= 99; i++)
-
-											{
-
-															if (i != j)
-
-																			{
-
-																								putchar(j / 10 + 48);
-
-																												putchar(j % 10 + 48);
-
-																																putchar(' ');
-
-																																				putchar(i / 10 + 48);
-
-																																								putchar(i % 10 + 48);
-
-																																											}
-
-																		if (j * 100 + i != 9899)
-
-																						{
-
-																											putchar(',');
-
-																															putchar(' ');
-
-																																		}
-
-																				}
-
-									}
-
-				putchar('\n');
-
-
-
-					return (0);
-
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }

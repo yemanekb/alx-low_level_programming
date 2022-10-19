b17c7af765bd34ef61152a9937b9921ca4fe6f6b
yemanekb <yemanekb@gmail.com>
2022-10-19 03:50:13 -0700

    bug fix for 0x01

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print all single digit numbers
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = o ; n < 10 ; n++)
+	{	
+		printf("%d", n);
+	}		
+	putchar('\'n);
+	return (0);
+}

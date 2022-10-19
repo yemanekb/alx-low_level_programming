46bb8b5fd9abfd534600a6014230e5acfc5a8bb0
yemanekb <yemanekb@gmail.com>
2022-10-19 11:59:54 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -12,11 +12,11 @@ int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return(1);
+		return (1);
 	}
-	else if(c >= 65 && c <= 90)
+	else if (c >= 65 && c <= 90)
 	{
-		return(1);
+		return (1);
 	}
 	return (0);
 }

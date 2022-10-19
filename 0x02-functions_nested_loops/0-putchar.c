23ce9eea4c7f0d87383db5c109bf06dbc83e5cbf
yemanekb <yemanekb@gmail.com>
2022-10-19 05:41:14 -0700

    Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * main -  Entry point
  *
@@ -8,13 +9,15 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
 }

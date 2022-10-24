2b24287159ca458312f5427c7dbadb3fdab6b5d2
yemanekb <113693054+yemanekb@users.noreply.github.com>
2022-10-24 21:51:10 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,45 +1,23 @@
 #include "main.h"
-
 #include <stdio.h>
 
-
-
 /**
- *
- *  * print_array - print an array.
- *
- *   * @a: the array to print.
- *
- *    * @n: array's length
- *
- *     */
-
-
+ * print_array - print an array.
+ * @a: the array to print.
+ * @n: array's length
+ */
 
 void print_array(int *a, int n)
-
 {
+	int i;
 
-		int i;
-
-
-
-			for (i = 0; i < n; i++)
-
-					{
-
-								printf("%d", a[i]);
-
-										if (i < n - 1)
-
-													{
-
-																	printf(", ");
-
-																			}
-
-											}
-
-				printf("\n");
-
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }

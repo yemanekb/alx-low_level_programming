4da8a8e6185f206fdc5c813b53e87aab409c1427
yemanekb <113693054+yemanekb@users.noreply.github.com>
2022-10-24 22:07:09 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,77 +1,39 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <math.h>
 
-
-
 /**
+ * main - print password.
  *
- *  * main - print password.
- *
- *   *
- *
- *    * Return: 0.
- *
- *     */
-
-
+ * Return: 0.
+ */
 
 int main(void)
-
 {
+	int ascii = 2772, i = 0, j, random;
+	char password[100];
+	time_t t;
 
-		int ascii = 2772, i = 0, j, random;
+	srand((int) time(&t));
+	while (ascii > 126)
+	{
+		random = rand() % 126;
+		password[i] = random;
+		ascii -= random;
+		i++;
+	}
+	if (ascii > 0)
+		password[i] = ascii;
+	else
+	{
+		i--;
+	}
+	
 
-			char password[100];
-
-				time_t t;
-
-
-
-					srand((int) time(&t));
-
-						while (ascii > 126)
-
-								{
-
-											random = rand() % 126;
-
-													password[i] = random;
-
-															ascii -= random;
-
-																	i++;
-
-																		}
-
-							if (ascii > 0)
-
-										password[i] = ascii;
-
-								else
-
-										{
-
-													i--;
-
-														}
-
-									
-
-
-
-									for (j = 0; j <= i; j++)
-
-											{
-
-														printf("%c", password[j]);
-
-															}
-
-										return (0);
-
+	for (j = 0; j <= i; j++)
+	{
+		printf("%c", password[j]);
+	}
+	return (0);
 }

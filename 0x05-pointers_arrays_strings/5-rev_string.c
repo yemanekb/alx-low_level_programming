496c96bacf4deb293c03bc3bcd4e69c445b04428
yemanekb <113693054+yemanekb@users.noreply.github.com>
2022-10-24 21:39:33 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,59 +1,30 @@
 #include "main.h"
 
-
-
 /**
+ * rev_string - reverse a string.
  *
- *  * rev_string - reverse a string.
- *
- *   *
- *
- *    *@s: the string to be reversed.
- *
- *     */
-
-
+ *@s: the string to be reversed.
+ */
 
 void rev_string(char *s)
-
 {
+	int length, j, i;
+	char v1, v2;
 
-		int length, j, i;
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
 
-			char v1, v2;
+	j = length - 1;
+	i = 0;
 
-
-
-				for (length = 0; s[length] != '\0'; length++)
-
-						{
-
-								}
-
-
-
-					j = length - 1;
-
-						i = 0;
-
-
-
-							while (j > i)
-
-									{
-
-												v1 = s[i];
-
-														v2 = s[j];
-
-																s[i] = v2;
-
-																		s[j] = v1;
-
-																				j--;
-
-																						i++;
-
-																							}
-
+	while (j > i)
+	{
+		v1 = s[i];
+		v2 = s[j];
+		s[i] = v2;
+		s[j] = v1;
+		j--;
+		i++;
+	}
 }

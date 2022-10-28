54d2a8ad378f08b36ed2d95be22007eda24b421c
yemanekb <yemanekb@gmail.com>
2022-10-28 00:15:57 -0700

    main

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,18 +7,17 @@
  */
 char *rot13(char *s)
 {
-	int i=0;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-			       	(s[i] >= 'A' && s[i] <= 'Z'))
+		while ((s[i] >= 'a' && s[i] <= 'z') ||							(s[i] >= 'A' && s[i] <= 'Z'))
 		{
 			if ((s[i] >= 'a' && s[i] <= 'm') ||
 					(s[i] >= 'A' && s[i] <= 'M'))
 				s[i] += 13;
 			else
-				s[i] -=13;
+				s[i] -= 13;
 			i++;
 		}
 		i++;

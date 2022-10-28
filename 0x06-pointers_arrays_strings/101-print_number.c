fea108658038d8550e54f756b612b2b5233db007
yemanekb <113693054+yemanekb@users.noreply.github.com>
2022-10-28 14:42:10 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,27 +1,25 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
-  * print_number - print numbers chars
-  * @n: integer params
-  * Return: 0
- **/
-
+ *print_number - prints an integer
+ *
+ *@n:integer params
+ *
+ *Return: 0
+ *
+ */
 void print_number(int n)
 {
-unsigned int n1;
+	unsigned int x;
 
-n1 = n;
-
-if (n < 0)
-{
-_putchar('-');
-n1 = -n;
-}
-
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
+	x = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -n;
+	}
+	if (x / 10 != 0)
+	{
+		print_number(x / 10);
+	}
+	_putchar((x % 10) + '0');
 }

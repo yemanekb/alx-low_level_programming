15d58a8caaca98a05ac0d8c192b0c61af7c1ed2b
yemanekb <113693054+yemanekb@users.noreply.github.com>
2022-11-16 21:09:57 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,149 +1,75 @@
-nclude "3-calc.h"
-
+#include "3-calc.h"
 #include <stdlib.h>
-
 #include <stdio.h>
 
-
-
 /**
+ * op_add - ...
+ * @a: ...
+ * @b: ...
  *
- *  * op_add - ...
- *
- *   * @a: ...
- *
- *    * @b: ...
- *
- *     *
- *
- *      * Return: ...
- *
- *       */
-
+ * Return: ...
+ */
 int op_add(int a, int b)
-
 {
-
-		return (a + b);
-
+	return (a + b);
 }
 
-
-
 /**
+ * op_sub - ...
+ * @a: ...
+ * @b: ...
  *
- *  * op_sub - ...
- *
- *   * @a: ...
- *
- *    * @b: ...
- *
- *     *
- *
- *      * Return: ...
- *
- *       */
-
+ * Return: ...
+ */
 int op_sub(int a, int b)
-
 {
-
-		return (a - b);
-
+	return (a - b);
 }
 
-
-
 /**
+ * op_mul - ...
+ * @a: ...
+ * @b: ...
  *
- *  * op_mul - ...
- *
- *   * @a: ...
- *
- *    * @b: ...
- *
- *     *
- *
- *      *  Return: ...
- *
- *       */
-
+ *  Return: ...
+ */
 int op_mul(int a, int b)
-
 {
-
-		return (a * b);
-
+	return (a * b);
 }
 
-
-
 /**
+ * op_div - ...
+ * @a: ...
+ * @b: ...
  *
- *  * op_div - ...
- *
- *   * @a: ...
- *
- *    * @b: ...
- *
- *     *
- *
- *      *  Return: ...
- *
- *       */
-
+ *  Return: ...
+ */
 int op_div(int a, int b)
-
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-		if (b == 0)
-
-				{
-
-							printf("Error\n");
-
-									exit(100);
-
-										}
-
-
-
-			return (a / b);
-
+	return (a / b);
 }
 
-
-
 /**
+ * op_mod - ...
+ * @a: ...
+ * @b: ...
  *
- *  * op_mod - ...
- *
- *   * @a: ...
- *
- *    * @b: ...
- *
- *     *
- *
- *      * Return: ...
- *
- *       */
-
+ * Return: ...
+ */
 int op_mod(int a, int b)
-
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-		if (b == 0)
-
-				{
-
-							printf("Error\n");
-
-									exit(100);
-
-										}
-
-
-
-			return (a % b);
-
+	return (a % b);
 }

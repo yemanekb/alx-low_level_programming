622aed14917a045d3ed510bdfdac3732eb7861b9
yemanekb <113693054+yemanekb@users.noreply.github.com>
2022-11-29 13:59:28 +0300

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,45 +1,23 @@
 #include "lists.h"
-
 #include <stdlib.h>
 
-
-
 /**
- *
- *  * pop_listint - Delete the first element of a singly linked list.
- *
- *   * @head: Pointer to a list.
- *
- *    * Return: Integer if success.
- *
- *     **/
-
-
+ * pop_listint - Delete the first element of a singly linked list.
+ * @head: Pointer to a list.
+ * Return: Integer if success.
+ **/
 
 int pop_listint(listint_t **head)
-
 {
+	listint_t *tp;
+	int my_data;
 
-		listint_t *tp;
+	if (*head == NULL)
+		return (0);
 
-			int my_data;
-
-
-
-				if (*head == NULL)
-
-							return (0);
-
-
-
-					tp = *head;
-
-						*head = tp->next;
-
-							my_data = tp->n;
-
-								free(tp);
-
-									return (my_data);
-
+	tp = *head;
+	*head = tp->next;
+	my_data = tp->n;
+	free(tp);
+	return (my_data);
 }
